645b9f6f96a1c3b604a735b4511f8ed006a0e864
anuragpekka <anuragpekka@gmail.com>
2025-04-26 21:00:36 +0530

    first Version

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project: Medical Chatbot',
+    version= '0.0.0',
+    author= 'Anurag P Ekka',
+    author_email= 'anuragpekka@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
