8617f3a9d4510aee0eeeab5a6481f0ac6adbd319
anuragpekka <anuragpekka@gmail.com>
2025-04-30 18:46:39 +0530

    Added cicd

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = 'Generative AI Project: Medical Chatbot',
+    name = 'Generative AI Project: Healthcare Chatbot',
     version= '0.0.0',
     author= 'Anurag P Ekka',
     author_email= 'anuragpekka@gmail.com',
